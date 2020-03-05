987b1157ba890fb5823a57d92360273eb994385f
Ben Lambert <ben@joyxer.com>
2020-03-06 03:50:04 +0900

    Add one finger pan

--- src/hooks/useZoom.js
+++ src/hooks/useZoom.js
@@ -7,6 +7,9 @@ export default function useZoom({ minScale, maxScale, onScale }) {
   const initialBoundingRect = useRef(null)
   const firstTouch = useRef(null)
   const initialPinchLength = useRef(null)
+  const firstOneFingerTouch = useRef(null)
+  const currentMiddleTouchOnElement = useRef(null)
+  const currentPinchLength = useRef(null)
 
   const [{ scale, middleTouchOnElement, translateX, translateY }, set] = useSpring(() => ({
     scale: 1,
@@ -55,26 +58,44 @@ export default function useZoom({ minScale, maxScale, onScale }) {
   const handleTouchMove = useCallback(
     event => {
       if (firstTouch.current) {
-        const currentMiddleTouchOnElement = getMiddleTouchOnElement(
-          event.touches,
-          initialBoundingRect.current
-        )
+        if (event.touches.length === 1) {
+          if (!firstOneFingerTouch.current) {
+            firstOneFingerTouch.current = [event.touches[0].clientX, event.touches[0].clientY]
+          }
+          set({
+            scale: clamp(currentPinchLength.current / initialPinchLength.current, minScale, maxScale),
+            translateX: (currentMiddleTouchOnElement.current.clientX - firstTouch.current[0]) + (event.touches[0].clientX - firstOneFingerTouch.current[0]),
+            translateY: (currentMiddleTouchOnElement.current.clientY - firstTouch.current[1]) + (event.touches[0].clientY - firstOneFingerTouch.current[1]),
+            immediate: true,
+          })
+        } else {
+          currentMiddleTouchOnElement.current = getMiddleTouchOnElement(
+            event.touches,
+            initialBoundingRect.current
+          )
 
-        const [touch1, touch2] = event.touches
-        const currentPinchLength = getLengthOfLine(touch1, touch2)
+          const [touch1, touch2] = event.touches
+          currentPinchLength.current = getLengthOfLine(touch1, touch2)
 
-        set({
-          scale: clamp(currentPinchLength / initialPinchLength.current, minScale, maxScale),
-          translateX: currentMiddleTouchOnElement.clientX - firstTouch.current[0],
-          translateY: currentMiddleTouchOnElement.clientY - firstTouch.current[1],
-          immediate: true,
-        })
+          set({
+            scale: clamp(currentPinchLength.current / initialPinchLength.current, minScale, maxScale),
+            translateX: currentMiddleTouchOnElement.current.clientX - firstTouch.current[0],
+            translateY: currentMiddleTouchOnElement.current.clientY - firstTouch.current[1],
+            immediate: true,
+          })
+        }
       }
     },
     [set]
   )
 
   const handleTouchEnd = useCallback(() => {
+    if (event.touches.length !== 0) {
+      return
+    }
+
+    firstOneFingerTouch.current = null
+
     set({
       scale: 1,
       translateX: 0,
